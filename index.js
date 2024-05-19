603a55e01429d693d5d3abc64684c2f2ab530831
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-19 16:59:44 +0530

    Hoisting, null and undefined examples

---
+++ index.js
@@ -0,0 +1,86 @@
+//Mapping users to get username
+
+const users = [
+  {
+    id: 1,
+    name: "Vibhor",
+    age: 29,
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "Draco",
+    age: 25,
+    isActive: true,
+  },
+  {
+    id: 3,
+    name: "Deepak",
+    age: 24,
+    isActive: false,
+  },
+];
+
+const data = users
+  .sort((a, b) => (a.age > b.age ? 1 : -1))
+  // .filter((user) => user.isActive)
+  .map(
+    (user) => user.name + " " + user.age + " " + user.id + " " + user.isActive
+  );
+console.log(data);
+console.log(
+  "--------------------------------------------------------------------------"
+);
+
+// Difference bw null and undefined
+let var1;
+console.log(var1);
+console.log(typeof var1);
+
+let var2 = null;
+console.log(var2);
+console.log(typeof var2);
+console.log(
+  "--------------------------------------------------------------------------"
+);
+
+let fool = 1;
+console.log("---------foooooooo----", fool);
+//Outputs - cannot access fool before initialization.
+
+console.log("---------fooooooool1----", fool1);
+var fool1 = 1;
+
+console.log(
+  "--------------------------------------------------------------------------"
+);
+
+// Hoisting
+var a;
+function foo() {
+  a = 20;
+  console.log(a);
+  return a;
+}
+console.log(a);
+b = foo();
+console.log(a);
+//Output undefined 20 20 - a is already declared outside the function but assigned in the function.
+//So, it is hoisted and gets 20 as value.
+
+// Hoisting
+var a;
+function foo() {
+  var a = 20;
+  console.log(a);
+  return a;
+}
+console.log(a);
+b = foo();
+console.log(a);
+//Output undefined 20 undefined - a ia already declared outside, but again a is declared inside the function.
+//So the assigned value will not be hoisted and it will be undefined.
+
+console.log(
+  "--------------------------------------------------------------------------"
+);
