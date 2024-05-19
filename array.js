9d9eaedf9a69da2d5066e8ce38ec32aa39193ac3
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-19 21:58:20 +0530

    concatenating the arrays - different approaches

--- array.js
+++ array.js
@@ -18,3 +18,29 @@ const newAppend = append(arr, 3);
 console.log("New Array : ", newAppend);
 console.log("Original Array : ", arr1);
 //Output - [1, 2, 3, 3] with changing the elements in original array.
+
+console.log("------------------------------------------");
+
+//push method mutates array itself, thats why arrr1 got updated when .push() method is used.
+const mergeArray = (arrr1, arrr2) => {
+  arrr1.push(...arrr2);
+  return arrr1;
+};
+const arrr1 = [1, 2, 3];
+const arrr2 = [4, 5, 6];
+const newMergeArray = mergeArray(arrr1, arrr2);
+console.log("NewArray :", newMergeArray); //Output - [1, 2, 3, 4, 5, 6]
+console.log("Old Array1 : ", arr1); //Output - [1, 2, 3]
+
+const mergeArrays = (ar, ar1) => {
+  return ar.concat(...ar1);
+  // return [...ar, ...ar1]; // Same approach without mutating the array.
+};
+const ar = [1, 2, 3];
+const ar1 = [4, 5, 6];
+const newMergeArrays = mergeArrays(ar, ar1);
+console.log("NewArray : ", newMergeArray); //Output - [1, 2, 3, 4, 5, 6]
+console.log("Old Array 1 : ", ar); //Output - [1,2,3]
+console.log("Old Array 2 : ", ar1); // Output - [4,5,6]
+
+console.log("------------------------------------------");
